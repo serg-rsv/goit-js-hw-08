57418bfb3c9f59186875595167b21b31ae468a24
Serhii Rybak <rybaksw@gmail.com>
2022-04-17 22:16:40 +0300

    task-01
    add SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.insertAdjacentHTML('afterbegin', createGalleryMarkup(galleryItems));
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ description, original, preview }) => {
+      return `
+      <li>
+        <a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>
+        `;
+    })
+    .join('');
+}
+
+const imgGallery = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
