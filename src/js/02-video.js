9c3c2df4b16eef69366a54d01029265f74c4540a
Serhii Rybak <rybaksw@gmail.com>
2022-04-19 17:19:55 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import throttle from 'lodash.throttle';
 
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+function onTimeUpdate(e) {
+  localStorage.setItem(CURRENT_TIME_KEY, e.seconds);
+}
+
+player.setCurrentTime(
+  localStorage.getItem(CURRENT_TIME_KEY) ? localStorage.getItem(CURRENT_TIME_KEY) : 0,
+);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
